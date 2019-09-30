964ad04a48007b244d410eab9253e5f3b6167311
Jordan Lamb <jdawg2329@gmail.com>
2019-09-30 10:47:38 -0600

    Initial Commit

---
+++ DeckOfOneCard/DeckOfOneCard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DeckofOneCard
+//
+//  Created by Jordan Lamb on 9/30/19.
+//  Copyright © 2019 Squanto Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
